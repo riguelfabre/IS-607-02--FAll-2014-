ba170e0add76353b2194c72ef2acd12baed11299
riguelfabre <riguelfabre@gmail.com>
2014-08-30 16:48:15 -0400

    HW 1
    Riguel Fabre HW 1 Week 1

---
+++ Riguel Fabre Week1 Hw.R
@@ -0,0 +1,15 @@
+#1.What versions of R and RStudio do you have installed?
+R version 3.1.1 (2014-07-10) -- "Sock it to Me"
+Copyright (C) 2014 The R Foundation for Statistical Computing
+Platform: i386-w64-mingw32/i386 (32-bit)
+
+
+
+#2.What version of PostgreSQL do you have installed?
+The version im using is PostgreSQL 9.3
+
+
+
+
+#3.Install and load the R package DMwR. Load the data set sales and determine the number of observations contained in the data set. (In RStudio, this is easy to determine.)
+401146 obs. of 5 variables
