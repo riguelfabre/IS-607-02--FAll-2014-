21e808a442dd47c6379ef056193387281de38b2b
riguelfabre <riguelfabre@gmail.com>
2014-10-06 22:48:16 -0400

    Week 6 HW 6

--- [IS607]Riguel Fabre Week 6 Hw.R
+++ [IS607]Riguel Fabre Week 6 Hw.R
@@ -3,8 +3,8 @@ library(rvest)
 
 
 
-HouseTv <- html("http://espn.go.com/mlb/player/stats/_/id/3246/derek-jeter")
-table <- HouseTv %>% 
+Stats <- html("http://espn.go.com/mlb/player/stats/_/id/3246/derek-jeter")
+table <- Stats %>% 
   html_nodes("div p") %>%
   html_text() %>%
   table
